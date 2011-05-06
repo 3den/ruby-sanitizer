29d17ad669f92ab4ca7c34444dafbbf71c619e2a
Marcelo Eden <edendroid@gmail.com>
2011-05-06 14:53:03 -0300

    Sanitizer libs and tests

--- Rakefile
+++ Rakefile
@@ -17,8 +17,8 @@ Jeweler::Tasks.new do |gem|
   gem.name = "sanitizer"
   gem.homepage = "http://github.com/3den/sanitizer"
   gem.license = "MIT"
-  gem.summary = %Q{TODO: one-line summary of your gem}
-  gem.description = %Q{TODO: longer description of your gem}
+  gem.summary = %Q{The simplest string cleaner ever made}
+  gem.description = %Q{Sanitizer.clean(text)}
   gem.email = "edendroid@gmail.com"
   gem.authors = ["Marcelo Eden"]
   # dependencies defined in Gemfile
--- spec/sanitizer_spec.rb
+++ spec/sanitizer_spec.rb
@@ -1,7 +1,47 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Sanitizer" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Sanitizer do
+  
+  describe "sanitize" do
+   
+    it "should strip all tags" do
+      html = "<div><p>Oi <b>como</b> <a href='/xxx/'>Vai</a></p><!-- s --></div>" 
+      output = Sanitizer.sanitize(html)
+      output.should == 'Oi como Vai'
+    end
+    
+    it "should clean spaces and tags" do
+      html = "<p>Oi <b>como</b>     
+    Vai</p>"
+      output = Sanitizer.sanitize(html)
+      output.should == 'Oi como Vai'
+    end
+    
+    it "should clean '&' entries" do
+      html = "Eu & você"
+      output = Sanitizer.sanitize(html)
+      output.should == 'Eu &amp; você'
+    end
+    
+    it "should not remove valid entries" do
+      html = "Eu &amp; você"
+      output = Sanitizer.sanitize(html)
+      output.should == 'Eu &amp; você'
+    end
+  end
+  
+  describe "strip_tags" do
+    
+    it "should remove only <b> tags" do
+       html = "<p>Oi <b>como</b> <a href='/xxx/'>Vai</a></p><!-- s -->" 
+      output = Sanitizer.strip_tags(html, 'b')
+      output.should == "<p>Oi como <a href='/xxx/'>Vai</a></p><!-- s -->"
+    end
+    
+    it "should remove only <b> and <a> tags" do
+       html = "<p>Oi <b>como</b> <a href='/xxx/'>Vai</a></p><!-- s -->" 
+      output = Sanitizer.strip_tags(html, 'a', 'b')
+      output.should == "<p>Oi como Vai</p><!-- s -->"
+    end
   end
 end
